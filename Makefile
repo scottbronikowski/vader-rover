58a7c725be5af4ce68d1e5f339c41b99e4ecca48
Scott Bronikowski <scottbronikowski@gmail.com>
2014-03-14 17:27:26 -0400

    -- converted camera-server.c to cpp in order to use flycap libraries -- copied flycap headers and so files from vader-ubuntu, but wouldn't work bc version of flycap was too new (GLIBC conflict)-->put these files in ~/vader-rover/OLD/ -- tried copying newer version of glibc (libc-2.15.so and ld-2.15.so) from vader-ubuntu, but this got really hairy really quick.  Then started on a full install of glibc-2.14.tar.gz into ~/vader-rover IOT use newer GLIBC, but abandoned this bc next bullet got working -- installed flycapture2-2.3.3.18 from ~/darpa-collaboration/bin/build-point-grey.sh in order to avoid conflict with GLIBC version (we have 2.13, flycapture2-2.5+ needs 2.14) -- created symlinks from ~/vader-rover to lib and include for flycapture    include -> /home/sbroniko/darpa-collaboration/include/x86_64-Linux-3.2.0-4-amd64/flycapture/     lib -> /home/sbroniko/darpa-collaboration/lib/x86_64-Linux-3.2.0-4-amd64/ --added 'export LD_LIBRARY_PATH=/home/sbroniko/darpa-collaboration/lib/x86_64-Linux-3.2.0-4-amd64/arv:/home/sbroniko/lib/x86_64-Linux-3.2.0-4-amd64/:/home/sbroniko/darpa-collaboration/lib/x86_64-Linux-3.2.0-4-amd64/arv:/home/sbroniko/lib/x86_64-Linux-3.2.0-4-amd64/:/home/sbroniko/lib/x86_64-Linux-3.2.0-4-amd64:/bin/glnx86/:/bin/glnxa64:/usr/local/lib/:/home/sbroniko/darpa-collaboration/lib/x86_64-Linux-3.2.0-4-amd64/:/usr/local/cuda/lib64/:/usr/share/cuda-toolkit/lib64/:/bin/glnx86/:/bin/glnxa64:/usr/local/lib/:~/darpa-collaboration/lib/x86_64-Linux-3.2.0-4-amd64/:/usr/local/cuda/lib64/:/usr/share/cuda-toolkit/lib64/' to .bashrc in order to find flycapture libraries at runtime -- started on building struct and receiver function for raw PGR image...look at using Image() constructor in Image.h where I can pass all paramaters directly.

--- Makefile
+++ Makefile
@@ -1,15 +1,29 @@
 #makefile for vader-rover stuff
 
 CC = gcc
-INCLUDE = 
-LIBS = 
-FLAGS = -Wall -g
+CC++ = g++
+VADER = /home/sbroniko/vader-rover
+INCLUDE = -I${VADER}/include 
+LIBS = -L${VADER}/lib -lflycapture${D} `imlib2-config --cflags --libs`
+FLAGS = -Wall
+
 
 camera-server: camera-server.o
-	${CC} -o camera-server camera-server.o ${LIBS} ${COMMON_LIBS}
+	${CC++} -o camera-server camera-server.o ${LIBS} ${COMMON_LIBS} 
 
-camera-server.o: camera-server.c
-	${CC} ${FLAGS} ${INCLUDE} -c camera-server.c
+
+#-Wl,--rpath=${HOME}/vader-rover/lib/glibc/ 
+#-WL,--dynamic-linker=${HOME}/vader-rover/lib/glibc/ld-linux-x86-64.so.2 
+#--verbose
+
+camera-server.o: camera-server.cpp
+	${CC++} ${FLAGS} ${INCLUDE} -c camera-server.cpp
+
+# camera-server: camera-server.o
+# 	${CC} -o camera-server camera-server.o ${LIBS} ${COMMON_LIBS}
+
+# camera-server.o: camera-server.c
+# 	${CC} ${FLAGS} ${INCLUDE} -c camera-server.c
 
 clean_obj:
 	rm -f camera-server.o
---
+++ camera-server.cpp
@@ -0,0 +1,359 @@
+/*
+Implementation of server to receive camera data from rover
+
+Code adapted from http://www.beej.us/guide/bgnet/output/html/multipage/clientserver.html
+
+Author: Scott Bronikowski
+Date: 11 March 2014
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <Imlib2.h>
+
+#include "FlyCapture2.h"
+using namespace FlyCapture2;
+
+//globals
+const char* k_FrontCamPort = "3601";
+const char* k_PanoCamPort = "3602";
+const int BACKLOG = 5;
+
+//structures
+struct PointGrey_t2 {
+  Image rawImage, convertedImage;
+  PixelFormat pixFormat;
+  BayerTileFormat bayerFormat;
+  unsigned char* pData;
+  unsigned int rows, cols, stride, dataSize;
+  Imlib_Image finalImage;
+};
+
+//prototypes
+void sigchld_handler(int s);
+void *get_in_addr(struct sockaddr *sa);
+int StartServer(const char* PORT);
+int AcceptConnection(int sockfd);
+int recvall(int s, unsigned char* buf, int* len);
+
+int main(int argc, char** argv)
+{
+  /* input error checking */
+  if (argc != 2)
+    {
+      fprintf(stderr, "Usage: %s portnumber (%s or %s)\n",
+	      argv[0], k_FrontCamPort, k_PanoCamPort);
+      exit(1);
+    }
+  if ((strcmp(argv[1], k_FrontCamPort) != 0) &&
+      (strcmp(argv[1], k_PanoCamPort) != 0))
+    {
+      fprintf(stderr, "Invalid port number chosen\n");
+      exit(1);
+    }
+  /* input is valid if we get here */
+      
+      
+  int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+  char* PORT = argv[1];
+  /* struct sockaddr_storage their_addr; // connector's address information */
+  /* socklen_t sin_size; */
+  struct sigaction sa;
+  /* char s[INET6_ADDRSTRLEN]; */
+  
+  sockfd = StartServer(PORT);
+  //printf("sockfd = %d, port = %s\n",sockfd,PORT);
+  
+  sa.sa_handler = sigchld_handler; // reap all dead processes
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = SA_RESTART;
+  if (sigaction(SIGCHLD, &sa, NULL) == -1) 
+    {
+      perror("sigaction");
+      exit(1);
+    }
+  
+  printf("server: waiting for connections...\n");
+  
+  //int i = 0;
+  
+  while(1) 
+    {  // main accept() loop
+      new_fd = AcceptConnection(sockfd);
+      if (new_fd == -1)
+	{
+	  printf("Error accepting connection\n");
+	  break;
+	}
+      if (!fork()) 
+	{ // this is the child process
+	  //printf("forking %d, new_fd = %d\n",i,new_fd);
+	  //i++;
+	  close(sockfd); // child doesn't need the listener
+
+	  //here's where we do the magic
+	  PointGrey_t2* PG = new PointGrey_t2;
+	  int img_size, retval;
+	  while(1)
+	    {
+	      //first receive dimensions/metadata
+	      retval = recv(new_fd, &PG->cols, sizeof(PG->cols), 0);
+	      if (retval < 0)
+	  	{
+	  	  printf("Error receiving cols\n");
+	  	  break;
+	  	}
+	      if (retval == 0)
+	  	{
+	  	  printf("Sender stopped sending\n");
+	  	  break;
+	  	}
+	      if (recv(new_fd, &PG->rows, sizeof(PG->rows),0) <= 0)
+		{
+		  printf("Error receiving rows\n");
+		}
+	      if (recv(new_fd, &PG->stride, sizeof(PG->stride),0) <= 0)
+		{
+		  printf("Error receiving stride\n");
+		}
+	      if (recv(new_fd, &PG->dataSize, sizeof(PG->dataSize),0) <= 0)
+		{
+		  printf("Error receiving dataSize\n");
+		}
+	      if (recv(new_fd, &PG->pixFormat, sizeof(PG->pixFormat),0) <= 0)
+		{
+		  printf("Error receiving pixFormat\n");
+		}
+	      if (recv(new_fd, &PG->bayerFormat, sizeof(PG->bayerFormat),0) <= 0)
+		{
+		  printf("Error receiving bayerFormat\n");
+		}
+	      //check received data
+	      printf("rows = %u, cols = %u, stride = %u, dataSize = %u\n",
+		     PG->rows, PG->cols, PG->stride, PG->dataSize);
+	      printf("pixFormat = %u, bayerFormat = %u\n", PG->pixFormat,
+		     PG->bayerFormat);
+	      //then receive data
+	      img_size = (int)(PG->dataSize);
+
+	  //     unsigned char* buf = (unsigned char*) malloc(img_size);
+	  //     if (recvall(new_fd, buf, &img_size) != 0)
+	  // 	{
+	  // 	  printf("Error in recvall\n");
+	  // 	  break;
+	  // 	}
+	  //     printf("Received %d bytes of image data\n", img_size);
+	  //     free(buf);
+
+	    }
+	  
+	  /* second draft*/
+	  // unsigned int width, height, depth;
+	  // int img_size, retval;
+	  // while(1)
+	  //   {
+	  //     //first receive size (w, h, d)
+	  //     retval = recv(new_fd, &width, sizeof(width), 0);
+	  //     if (retval < 0)
+	  // 	{
+	  // 	  printf("Error receiving width\n");
+	  // 	  break;
+	  // 	}
+	  //     if (retval == 0)
+	  // 	{
+	  // 	  printf("Sender stopped sending\n");
+	  // 	  break;
+	  // 	}
+	  //     if (recv(new_fd, &height, sizeof(height), 0) <= 0)
+	  // 	{
+	  // 	  printf("Error receiving height\n");
+	  // 	}
+	  //     if (recv(new_fd, &depth, sizeof(depth), 0) <= 0)
+	  // 	{
+	  // 	  printf("Error receiving depth\n");
+	  // 	}
+	  //     printf("Received w x h x d = %u x %u x %u\n", width, height, depth);
+
+	  //     //then receive data
+	  //     img_size = (int)(width * depth * height);
+	  //     unsigned char* buf = (unsigned char*) malloc(img_size);
+	  //     if (recvall(new_fd, buf, &img_size) != 0)
+	  // 	{
+	  // 	  printf("Error in recvall\n");
+	  // 	  break;
+	  // 	}
+	  //     printf("Received %d bytes of image data\n", img_size);
+	  //     free(buf);
+	  //   }
+	  /* end second draft*/
+
+
+	  /* /\* first draft -- using strings with length sent before string *\/ */
+	  /* int buflen, retval; */
+	  /* while(1) */
+	  /*   { */
+	  /*     //first receive size */
+	  /*     retval = recv(new_fd, &buflen, sizeof(buflen), 0); */
+	  /*     if (retval < 0) */
+	  /* 	{ */
+	  /* 	  printf("Error receiveing buflen\n"); */
+	  /* 	  break; */
+	  /* 	} */
+	  /*     if (retval == 0) */
+	  /* 	{ */
+	  /* 	  printf("Sender stopped sending\n"); */
+	  /* 	  break; */
+	  /* 	} */
+	  /*     printf("received buflen: %d\n", buflen); */
+	  /*     //then receive data */
+	  /*     unsigned char* buf = malloc(buflen); */
+	  /*     if (recvall(new_fd, buf, &buflen) != 0) */
+	  /* 	{ */
+	  /* 	  printf("Error in recvall\n"); */
+	  /* 	  break; */
+	  /* 	} */
+	  /*     printf("received buf: %s\n", buf); */
+	  /*     free(buf); */
+	  /*   } */
+	  /* /\* end first draft *\/ */
+	  
+	  /* if (send(new_fd, "Hello, world!", 13, 0) == -1) */
+	  /* 	perror("send"); */
+	  
+	  //printf("closed sockfd\n");
+	  close(new_fd);
+	  delete PG;
+	  printf("exiting from fork\n");
+	  exit(0);
+	}
+      //printf("after fork\n");
+      close(new_fd);  // parent doesn't need this
+    }
+  
+  return 0;
+}
+
+void sigchld_handler(int s)
+{
+  while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+
+void *get_in_addr(struct sockaddr *sa)
+{// get sockaddr, IPv4 or IPv6:
+  if (sa->sa_family == AF_INET) 
+    { 
+      return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+int StartServer(const char* PORT)
+{
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int yes=1;
+  int rv;
+  
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE; // use my IP
+  
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+    {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return -1;
+    }
+  
+  // loop through all the results and bind to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) 
+    {
+      if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			   p->ai_protocol)) == -1) 
+	{
+	  perror("server: socket");
+	  continue;
+	}
+    
+      if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+		     sizeof(int)) == -1) 
+	{
+	  perror("setsockopt");
+	  exit(1);
+	}
+    
+      if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) 
+	{
+	  close(sockfd);
+	  perror("server: bind");
+	  continue;
+	}
+      
+      break;
+    }
+  
+  if (p == NULL)  
+    {
+      fprintf(stderr, "server: failed to bind\n");
+      return -1;
+    }
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  if (listen(sockfd, BACKLOG) == -1) 
+    {
+      perror("listen");
+      exit(1);
+    } 
+  return sockfd;
+}
+
+int AcceptConnection(int sockfd)
+{
+  struct sockaddr_storage their_addr; // connector's address information
+  socklen_t sin_size;
+  int new_fd;
+  char s[INET6_ADDRSTRLEN];
+
+  sin_size = sizeof(their_addr);
+  new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+  if (new_fd == -1) 
+    {
+      perror("accept");
+      return new_fd;
+    }
+  
+  inet_ntop(their_addr.ss_family,
+	    get_in_addr((struct sockaddr *)&their_addr),
+	    s, sizeof(s));
+  printf("server: got connection from %s\n", s);
+  return new_fd;
+}
+
+int recvall(int s, unsigned char* buf, int* len)
+{
+  int total = 0;        // how many bytes we've received
+  int bytesleft = *len; // how many we have left to receive
+  int n;
+  
+  while(total < *len) {
+    n = recv(s, buf+total, bytesleft, 0);
+    if (n == -1) { break; }
+    total += n;
+    bytesleft -= n;
+  }
+  
+  *len = total; // return number actually sent here
+  //printf("received %d\n", *len);
+  return n==-1?-1:0; // return -1 on failure, 0 on success
+}
